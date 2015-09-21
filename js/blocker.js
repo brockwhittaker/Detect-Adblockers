318f72af6dcec74ae8666c9f6c34eb9d73ffb65d
brockwhittaker <bjwhitta@asu.edu>
2015-09-21 05:22:51 -0700

    The basic adblocker detector.
    This is the initial commit.

---
+++ js/blocker.js
@@ -0,0 +1,54 @@
+function $id (id) {
+  return document.getElementById(id);
+}
+
+function AdsRemoved () {
+  this.AdsRemoved = false;
+  this.id = "adBlockEnabled";
+  return this;
+}
+
+AdsRemoved.prototype = {
+  checkScripts: function () {
+    if (typeof adsEnabled === "undefined") {
+      this.AdsRemoved = true;
+    }
+    return this;
+  },
+  createiFrame: function () {
+    var n = document.createElement('iframe');
+    n.src = "http://ad.mo.doubleclick.net/dartproxy/dfa.click.handler?";
+    n.id = this.id;
+    n.style.visibility = "hidden";
+    n.style.height = "0px";
+    n.style.width = "0px";
+    document.body.appendChild(n);
+    return this;
+  },
+  removeiFrame: function () {
+    var n = $id(this.id), $this = this;
+    n.onload = function () {
+      if ($id($this.id).style.display == "none") {
+        $this.AdsRemoved = true;
+      } else {
+        document.body.removeChild($id($this.id));
+      }
+      delete $this.id;
+    };
+    return this;
+  },
+  ifAdsDisabled: function (func) {
+    if (this.AdsRemoved === true)
+      func();
+  },
+  ifAdsEnabled: function (func) {
+    if (this.AdsRemoved === false)
+      func();
+  },
+  run: function () {
+    this.checkScripts().createiFrame().removeiFrame();
+  }
+};
+
+var ads = new AdsRemoved();
+ads.run();
